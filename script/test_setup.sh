6f3bcbdb50164d5d914ad52a1d7bf2ad3ca12413
tommytim0515 <xtianae@connect.ust.hk>
2022-08-08 02:09:50 +0800

    test network bash script

--- script/test_setup.sh
+++ script/test_setup.sh
@@ -6,6 +6,7 @@ export FABRIC_CFG_PATH=${PWD}/../config
 cd ..
 DIR_PATH=${PWD}
 echo ${PWD}
+rm -rf tmp
 mkdir -p tmp
 cd tmp
 
@@ -36,3 +37,21 @@ export CORE_PEER_LOCALMSPID="Org1MSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 export CORE_PEER_ADDRESS=localhost:7051
+export CORE_CHAINCODE_ID_NAME=basic
+
+cd ${DIR_PATH}/tmp
+if [ -d "local-chain" ]
+then
+    echo "local-chain logic exists"
+    cd local-chaincode
+    git stash
+    git pull --rebase
+else
+    echo "local-chain logic does not exist"
+    git clone https://github.com/ttvs-blockchain/local-chain.git
+    cd local-chain
+fi
+
+go build -o local-chain
+chmod +x local-chain
+./local-chain
